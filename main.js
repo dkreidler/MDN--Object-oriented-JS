1e026ea1e65df7618c8591a84e126eb5e15f2953
dkreidler <dkreidler@gmail.com>
2018-07-04 13:39:35 -0400

    Refactored into two files, just because
    Just keeping it fresh.

---
+++ main.js
@@ -0,0 +1,20 @@
+var person = {
+    name: {
+        first: 'Bob',
+        last: 'Smith'
+    },
+    age: 32,
+    gender: 'male',
+    interests: ['music', 'skiing'],
+    bio: function() {
+        alert(this.name[0] + ' ' + this.name[1] + ' is ' + this.age + ' years old. He likes ' + this.interests[0] + ' and ' + this.interests[1] + '.');
+
+    },
+    greeting: function() {
+        alert('Hi! I\'m ' + this.name[0] + '.');
+    }
+
+};
+var myDataName = 'height';
+var myDataValue = '1.75m';
+person[myDataName] = myDataValue;
